ebfe5fadb4d83e5cf636e8cfe38c17a5b9e6c6cf
ssander5 <ssander5@nd.edu>
2023-06-08 16:54:08 -0400

    all directories non-empty

--- Setup/PullData.sh
+++ Setup/PullData.sh
@@ -1,3 +1,5 @@
+rm ../Input_data/README
+
 wget https://raw.githubusercontent.com/NCGAS/de-novo-transcriptome-assembly-pipeline/v5_dev/Project_v5/input_files/GSF1664-0-min-1neg_S10_R1_001.short.fastq -P ../Input_data
 wget https://raw.githubusercontent.com/NCGAS/de-novo-transcriptome-assembly-pipeline/v5_dev/Project_v5/input_files/GSF1664-0-min-1pos_S7_R1_001.short.fastq -P ../Input_data
 
